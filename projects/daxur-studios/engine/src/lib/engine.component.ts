e37e72c75aa8018f2baca5f25759cd07e3de0b34
Bruno Kertesz <daxur.studios@gmail.com>
2023-06-25 00:18:00 +0100

    first commit!

---
+++ projects/daxur-studios/engine/src/lib/engine.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'daxur-engine',
+  template: `
+    <p>
+      engine works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class EngineComponent {
+
+}
---
+++ projects/daxur-studios/engine/src/lib/engine.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { EngineComponent } from './engine.component';
+
+
+
+@NgModule({
+  declarations: [
+    EngineComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    EngineComponent
+  ]
+})
+export class EngineModule { }
